603421715e58b201cbc0b10a8e9cb219f9c933e4
lceberg <kaloyan.github@gmail.com>
2016-04-16 19:43:44 +0300

    Added pagination

--- fragments/loop.php
+++ fragments/loop.php
@@ -17,4 +17,22 @@ if ( have_posts() ):
 			</div>
 		</div>
 	<?php endwhile;
+
+	$max_pages = $wp_query->max_num_pages;
+	$paged     = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+
+	if ( $max_pages > 1 ): ?>
+		<div class="pagination">
+			<a href="<?php echo add_query_arg( 'paged', ( $paged - 1 ) > 0 ? $paged - 1 : 1 ); ?>">&laquo;</a>
+			<?php
+				foreach ( range( 1, $max_pages ) as $page_number ):
+					$page_class = ( $page_number == $paged ) ? 'current' : ''; ?>
+					<a class="<?php echo $page_class; ?>" href="<?php echo add_query_arg( 'paged', $page_number ); ?>">
+						<?php echo $page_number; ?>
+					</a>
+				<?php endforeach;
+			?>
+			<a href="<?php echo add_query_arg( 'paged', ( $paged + 1 ) < $max_pages ? $paged + 1 : $max_pages ); ?>">&raquo;</a>
+		</div>
+	<?php endif;
 endif;
